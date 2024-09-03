23b43717e6525076c13ce5895dd6a6db373484fb
sahand <sahandasg56@gmail.com>
2024-09-03 17:36:33 +0330

    develop logical part of project

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,21 +2,33 @@ import React, {Component} from 'react'
 
 export default class Todo extends Component {
 
+    checkedHandler(id) {
+        this.props.editHandler(id)
+    }
+
+    trashHandler(id) {
+        this.props.removeHandler(id)
+    }
+
     render() {
         return (
             // 'completed' class for completed todos
             <div
-                className="flex justify-center items-center m-6 bg-white text-2xl transition ease-in delay-[.5ms] text-[#585757FF]">
-                <li className="py-0 px-5 flex-1">Test</li>
+                className={this.props.completed ?
+                    "flex justify-between items-center m-6 bg-white text-2xl transition ease-in delay-[.5ms] text-[#585757FF] opacity-60 line-through"
+                    : "flex justify-between items-center m-6 bg-white text-2xl transition ease-in delay-[.5ms] text-[#585757FF]"}>
+                <li className="py-0 px-5 flex-1">{this.props.Title}</li>
 
                 <button
-                    className="bg-[#4464ADFF] text-white border-none p-4 cursor-pointer text-xl transition ease-in delay-[.2ms] hover:bg-[#4464ADFF] hover:text-white pointer-events-none"
+                    className="bg-[#4464ADFF] text-white border-none p-4 cursor-pointer text-xl  ease-in delay-[.2ms] hover:bg-[#4464ADFF] hover:text-white"
+                    onClick={this.checkedHandler.bind(this, this.props.id)}
                 >
                     <i className="fa fa-check" aria-hidden="true"></i>
                 </button>
 
                 <button
-                    className="bg-[#4A156EFF] text-white border-none p-4 cursor-pointer text-xl transition ease-in delay-[.2ms] hover:text-white hover:bg-[#4A156EFF] pointer-events-none">
+                    className="bg-[#4A156EFF] text-white border-none p-4 cursor-pointer text-xl transition ease-in delay-[.2ms] hover:text-white hover:bg-[#4A156EFF]"
+                    onClick={this.trashHandler.bind(this, this.props.id)}>
                     <i className="fa fa-trash" aria-hidden="true"></i>
                 </button>
             </div>
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -5,26 +5,96 @@ export default class TodoList extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            todos: [],
+            todoTitle: '',
+            status: 'all'
+        }
+
+        this.addTodo = this.addTodo.bind(this)
+        this.removeTodo = this.removeTodo.bind(this)
+        this.editTodo = this.editTodo.bind(this)
+        this.todoTitleHandler = this.todoTitleHandler.bind(this)
+        this.statusHandler = this.statusHandler.bind(this)
+
     }
+
+    todoTitleHandler(event) {
+        if (event.target.value) {
+            let newTodo = event.target.value
+            this.setState({
+                todoTitle: newTodo
+            })
+        }
+    }
+
+    addTodo(event) {
+        event.preventDefault()
+        if (this.state.todoTitle) {
+            this.setState({
+                todos: [...this.state.todos, {
+                    id: this.state.todos.length + 1,
+                    Title: this.state.todoTitle,
+                    completed: false
+                }],
+                todoTitle: ''
+            })
+        }
+    }
+
+    editTodo(id) {
+        let newTodos = [...this.state.todos];
+        newTodos.forEach(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+        })
+        this.setState({
+            todos: newTodos
+        })
+    }
+
+    removeTodo(id) {
+        let oldTodos = [...this.state.todos];
+        let newTodos = oldTodos.filter(todo => {
+            return todo.id !== id
+        })
+        this.setState({
+            todos: newTodos
+        })
+    }
+
+    statusHandler(event) {
+        this.setState({
+            status: event.target.value
+        })
+    }
+
     render() {
         return (
             <>
                 <form className='min-h-50vh flex justify-center items-center'>
                     <input
+                        onChange={this.todoTitleHandler}
                         type="text"
                         className="p-2 border-none text-3xl bg-[#f4f4f4] text-[#585757FF]"
                         maxLength="40"
+                        value={this.state.todoTitle}
                     />
                     <button
                         className="p-2 border-none text-3xl bg-white text-[#4A156EFF]
                         cursor-pointer transition ease-in delay-[.3ms] hover:bg-[#4A156EFF]
                         hover:text-white"
                         type="submit"
+                        onClick={this.addTodo}
                     >
                         <i className="fa fa-plus"></i>
                     </button>
-                    <div className="m-4 relative overflow-hidden after:content-[\25BC] after:absolute after:bg-[#4A156EFF] after:top-0 after:right-0 after:p-5 after:pointer-events-none after:transition after:ease-in after:delay-[.3ms] hover:after:bg-white hover:after:text-[#4A156EFF]">
-                        <select className="appearance-none outline-none border-none text-xl text-[#4A156EFF] w-40 cursor-pointer p-3" name="todos">
+                    <div
+                        className="m-4 relative overflow-hidden after:content-[\25BC] after:absolute after:bg-[#4A156EFF] after:top-0 after:right-0 after:p-5 after:pointer-events-none after:transition after:ease-in after:delay-[.3ms] hover:after:bg-white hover:after:text-[#4A156EFF]">
+                        <select
+                            className="appearance-none outline-none border-none text-xl text-[#4A156EFF] w-40 cursor-pointer p-3"
+                            name="todos" onChange={this.statusHandler}>
                             <option value="all">All</option>
                             <option value="completed">Completed</option>
                             <option value="uncompleted">Uncompleted</option>
@@ -34,10 +104,42 @@ export default class TodoList extends Component {
 
                 <div className="flex justify-center items-center">
                     <ul className="list-none min-w-[30%]">
-                        <Todo/>
+                        {
+                            this.state.status === 'all' && this.state.todos.map(todo => (
+                                <Todo
+                                    key={todo.id}
+                                    {...todo}
+                                    editHandler={this.editTodo}
+                                    removeHandler={this.removeTodo}
+                                />
+                            ))
+                        }
+                        {
+                            this.state.status === 'completed' && this.state.todos.filter(todo => (todo.completed))
+                                .map(todo=>(
+                                    <Todo
+                                    key={todo.id}
+                                    {...todo}
+                                    editHandler={this.editTodo}
+                                    removeHandler={this.removeTodo}
+                                />
+                                ))
+                        }
+                        {
+                            this.state.status === 'uncompleted' && this.state.todos.filter(todo => (!todo.completed))
+                                .map(todo=>(
+                                    <Todo
+                                    key={todo.id}
+                                    {...todo}
+                                    editHandler={this.editTodo}
+                                    removeHandler={this.removeTodo}
+                                />
+                                ))
+                        }
                     </ul>
                 </div>
             </>
         )
     }
 }
+
